efad97c6a423aa3bd7ce77e90d388ed29dbc3c7c
USER <nguyenmy494041@gmail.com>
2020-07-26 21:17:24 +0700

    baitapjson

--- Baitapmodule/Baitapmodule/Bai1.cs
+++ Baitapmodule/Baitapmodule/Bai1.cs
@@ -7,87 +7,116 @@ namespace Baitapmodule
 {
     class Bai1
     {
-        public CreateData input = new CreateData();
-        public Variable variables = new Variable();
-        const int numberVariable = 2;
-        const int numberObject = 2;
+        //public CreateData input = new CreateData();
+        //public Variable variables = new Variable();
+        const int numberVariable = 3;
+        const string parameter_1 = "a";
+        const string parameter_2 = "b";
+        const string parameter_3 = "c";
+
         static void Main(string[] args)
         {
-            var filePath = $@"C:\Users\USER\source\repos\Baitapmodule\Baitapmodule\Data\inputData.json";
-            using (StreamWriter sw = File.CreateText(filePath))
-            {
+            var filePath = $@"E:\CODEGYM\Module2\Baitapmodule\Baitapmodule\Data\inputData.json";
+            var outfilePath1 = $@"E:\CODEGYM\Module2\Baitapmodule\Baitapmodule\Data\output1.json";
+            var outfilePath2 = $@"E:\CODEGYM\Module2\Baitapmodule\Baitapmodule\Data\output2.json,";
+            var result = new Data();
 
-               
-                List<List<Parameter>> createdata = CreateVariable();
-                
-                var data = JsonConvert.SerializeObject(createdata);
+            // write data in inputData.json
+
+            //using (StreamWriter sw = File.CreateText(filePath))
+            //{
+            //    NewInput newInput = new NewInput();
+            //    newInput.inputData = CreateInputData();
+            //    var datainput = JsonConvert.SerializeObject(newInput);
+            //    sw.Write(datainput);
+            //}
+
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                var data = sr.ReadToEnd();
+                result = JsonConvert.DeserializeObject<Data>(data);
+            }
+
+
+            // output1Data
+
+            var response = new Response();
+            response.outputData = result.inputData;
+            foreach (var std in response.outputData)
+            {
+                int sum = 0;
+                foreach (var item in std)
+                {
+                    sum += item.score;
+                }
+                std.Add(new Parameter() { name = $"Sum({parameter_1}, {parameter_2}, {parameter_3})", score = sum });
+            }
+
+            using (StreamWriter sw = File.CreateText(outfilePath1))
+            {
+                var data = JsonConvert.SerializeObject(response);
                 sw.Write(data);
             }
 
 
-        }
-        public void CreateData(StreamWriter sw)
-        {
 
+            // output2Data
+
+            //var response_1 = new Response();
+            //response_1.outputData = result.inputData;
+            //foreach (var std in response_1.outputData)
+            //{               
+            //    foreach (var item in std)
+            //    {
+            //       item.score *= 2;
+            //    }                
+            //}
+            //using (StreamWriter sw = File.CreateText(outfilePath2))
+            //{
+            //    var data = JsonConvert.SerializeObject(response_1);
+            //    sw.Write(data);
+            //}
+            //Console.ReadKey();
         }
-        public static List<List<Parameter>> CreateVariable()
+        
+        // method write data
+        public static List<List<Parameter>> CreateInputData()
         {
             List<List<Parameter>> createdata = new List<List<Parameter>>();
-
+            List<string> parameters = new List<string> { parameter_1, parameter_2, parameter_3 };
             for (int i = 0; i < numberVariable; i++)
             {
                 List<Parameter> variables = new List<Parameter>();
-                for (int j = 0; j < numberVariable; j++)
+                for (int j = 0; j < parameters.Count; j++)
                 {
+                    bool result; int number;
                     Parameter parameter = new Parameter();
-                    Console.Write("Input name properties: ");
-                    parameter.name = Console.ReadLine();
-                    Console.Write("Input value proerties: ");
-                    parameter.score = int.Parse(Console.ReadLine());
+                    parameter.name = parameters[j];
+                    do
+                    {
+                        Console.Write($"Input value {parameters[j]}: ");
+                        result = int.TryParse(Console.ReadLine(), out number);
+                    } while (!result);
+                    parameter.score = number;
                     variables.Add(parameter);
                 }
                 createdata.Add(variables);
+                Console.WriteLine();
             }
             return createdata;
         }
 
-        //public static CreateData CreateVariable()
-        //{
-        //    CreateData createdata = new CreateData();
 
-        //    for (int i = 0; i < numberVariable; i++)
-        //    {
-        //        Variable variables = new Variable();
-        //        for (int j = 0; j < numberVariable; j++)
-        //        {
-        //            Parameter parameter = new Parameter();
-        //            Console.Write("Input name properties: ");
-        //            parameter.name = Console.ReadLine();
-        //            Console.Write("Input value proerties: ");
-        //            parameter.score = int.Parse(Console.ReadLine());
-        //            variables.variables.Add(parameter);
-        //        }
-        //        createdata.data.Add(variables);
-        //    }
-        //    return createdata;
-        //}
     }
-    public class CreateData
+    public class NewInput
     {
-        public List<Variable> data { get; set; }
+        public List<List<Parameter>> inputData { get; set; }
     }
-    public class Variable
+
+
+    public class Data
     {
-        public List<Parameter> variables { get; set; }
-        public override string ToString()
-        {
-            string result = "";
-            foreach (var item in variables)
-            {
-                result += item.ToString() + "\n";
-            }
-            return result;
-        }
+        public List<List<Parameter>> inputData { get; set; }
     }
 
     public class Parameter
@@ -96,7 +125,14 @@ namespace Baitapmodule
         public int score { get; set; }
         public override string ToString()
         {
-            return $"{name} {score}";
+            return $"{name} = {score}";
         }
+
     }
+    public class Response
+    {
+        public List<List<Parameter>> outputData { get; set; }
+    }
+
+    
 }
---
+++ Baitapmodule/Baitapmodule/Bai2.cs
@@ -0,0 +1,174 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Baitapmodule
+{
+    class Bai2
+    {
+        const int numberStudent = 3;
+        static void Main()
+        {
+            var filePath = $@"E:\CODEGYM\Module2\Baitapmodule\Baitapmodule\Data\data.json";
+            var outfilePath = $@"E:\CODEGYM\Module2\Baitapmodule\Baitapmodule\Data\Outcome.json";
+            var result = new Students();
+
+            // write file data.json
+            using (StreamWriter sw = File.CreateText(filePath))
+            {
+                Students students = new Students();
+                students = CreateData();
+                var datainput = JsonConvert.SerializeObject(students);
+                sw.Write(datainput);
+            }
+
+
+            // read file data.json
+            using (StreamReader sr = File.OpenText(filePath))
+            {
+                var data = sr.ReadToEnd();
+                result = JsonConvert.DeserializeObject<Students>(data);
+            }
+
+
+            var processing = new ProcessingData()
+            {
+                students = new List<ResStudent>()
+            };
+           
+            foreach (var std in result.students)
+            {
+                processing.students.Add(new ResStudent()
+                {
+                    code = std.code,
+                    average = std.AverageScore(),
+                    gender = std.gender,
+                    fullname = std.fullname,
+                    subject = std.subject,
+                    rank = std.RankedAcademic(std.AverageScore())
+                });
+            }
+            processing.students.Sort(new AverageScoreComparer());
+            processing.students.Reverse();
+            using (StreamWriter sw = File.CreateText(outfilePath))
+            {
+                var data = JsonConvert.SerializeObject(processing);
+                sw.Write(data);
+            }
+          
+        }
+        static Students CreateData()
+        {
+            List<string> nameSubject = new List<string> { "Math", "Physical", "Chemistry" };
+            Students students = new Students();
+            for (int i = 0; i < numberStudent; i++)
+            {
+                Student student = new Student();
+                Console.Write($"Enter code student_{i + 1}: ");
+                student.code = Console.ReadLine();
+                Console.Write($"Enter fullname student_{i + 1}: ");
+                student.fullname = Console.ReadLine();
+                Console.Write($"Enter gender student_{i + 1}: ");
+                student.gender = Console.ReadLine();
+
+                foreach (var item in nameSubject)
+                {
+                    Subject subject = new Subject();
+                    bool result; float number;
+                    subject.name = item;
+                    do
+                    {
+                        Console.Write($"Enter score {item}: ");
+                        result = float.TryParse(Console.ReadLine(), out number);
+
+                    } while (!result || number <= 0 || number > 10.0);
+                    subject.score = number;
+
+                    student.subject.Add(subject);
+                }
+                students.students.Add(student);
+                Console.WriteLine();
+            }
+            return students;
+        }
+
+    }
+
+
+    public class Students
+    {
+        public List<Student> students { get; set; }
+        public Students()
+        {
+            students = new List<Student>();
+        }
+
+    }
+    public class Student
+    {
+        public string code { get; set; }
+        public string fullname { get; set; }
+        public string gender { get; set; }
+        public List<Subject> subject { get; set; }
+        public Student()
+        {
+            this.subject = new List<Subject>();
+        }
+        public override string ToString()
+        {
+            return $"{code}\t{fullname}\t{gender}\t\t{subject[0].score}" +
+                $"\t\t{subject[1].score}\t\t{subject[2].score}" +
+                $"\t{AverageScore()}\t{RankedAcademic(AverageScore())}";
+        }
+        public double AverageScore()
+        {
+            double total = 0.0f;
+            foreach (var item in subject)
+            {
+                if (item.name == "Math")
+                {
+                    total += item.score * 2;
+                }
+                else
+                {
+                    total += item.score;
+                }
+            }
+            return Math.Round(total / (subject.Count + 1), 2);
+        }
+        public string RankedAcademic(double ave)
+        {
+            if (ave >= 9.0f) return "Excellent";
+            if (ave >= 8.0f) return "Great";
+            if (ave >= 7.0f) return "Pretty";
+            if (ave >= 6.5f) return "Quite_good";
+            if (ave >= 5.0f) return "Average";
+            if (ave >= 3.5f) return "Weak";
+            return "Least";
+        }
+    }
+    public class Subject
+    {
+        public string name { get; set; }
+        public float score { get; set; }
+    }
+    public class ProcessingData
+    {
+        public List<ResStudent> students { get; set; }
+    }
+
+    public class ResStudent : Student
+    {
+        public double average { get; set; }
+        public string rank { get; set; }
+    }
+    public class AverageScoreComparer : IComparer<Student>
+    {
+        public int Compare(Student x, Student y)
+        {
+            return x.AverageScore().CompareTo(y.AverageScore());
+        }
+    }
+}
